7f3ee292526a122d1bec6079f924b52893a7626c
Yueen Ma <myueen@ad.unc.edu>
2025-04-13 21:15:46 -0400

    revised wip

--- revised/sleeping_barber2.go
+++ revised/sleeping_barber2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -9,9 +10,11 @@ import (
 var pid int = 0
 
 // channels
-var customer_arrive_ch = make(chan int)
-var waiting_room_ch = make(chan int, 6) // buffered channel for 6 customers
-var barber_cut_hair_ch = make(chan int)
+var customerArrivesCh = make(chan int)
+var waitingRoomCh = make(chan int, 6) // buffered channel for 6 customers
+var barberWorkingCh = make(chan int)
+var barberSleepCh = make(chan bool)
+var haircutComplete = make(chan int)
 
 func main() {
 	// Make waiting room, receptionist, barber
@@ -23,44 +26,69 @@ func main() {
 	for i := 0; i >= 0; i++ {
 		customer_pid := pid
 		pid++
-		fmt.Printf("Greeting Customer %d \n", customer_pid)
 		go customer(customer_pid)
-		time.Sleep(1 * time.Second)
+		r := rand.Intn(10)
+		time.Sleep(time.Duration(r) * time.Second)
 	}
 }
 
 func customer(customer_pid int) {
 	// Messages (wait, full, done)
 	// When arrive, sent to the receptionist
-	customer_arrive_ch <- customer_pid
+	customerArrivesCh <- customer_pid
+	fmt.Printf("Customer %d arrives \n", customer_pid)
 
+	// Check if customer is done
+	for {
+		var pid_done = <-haircutComplete
+		if customer_pid == pid_done {
+			fmt.Printf("Customer %d haircut complete \n", customer_pid)
+		} else {
+			haircutComplete <- pid_done
+		}
+	}
 }
 
 func receptionist() {
 	fmt.Printf("Receptionist created")
 	for {
-		var customer_pid = <-customer_arrive_ch
+		var customer_pid = <-customerArrivesCh
 		fmt.Printf("Greeting %d \n", customer_pid)
 
-		select {
-		case waiting_room_ch <- customer_pid:
+		if len(waitingRoomCh) < 6 {
+			waitingRoomCh <- customer_pid
 			fmt.Printf("Customer %d sent to waiting room \n", customer_pid)
-		default:
+		} else {
 			fmt.Printf("Waiting room full, customer %d sent away \n", customer_pid)
 		}
 	}
 	// Messages (receive_customer, send_customer_to_waiting_room, tell_barber_added, tell_customer_to_wait)
-
 }
 
 func waiting_room() {
 	fmt.Printf("Waiting room created")
 	// Messages (receive_customer, enqueue, dequeue, check_empty)
+	for {
+		var customer_pid = <-waitingRoomCh
+		fmt.Printf("Customer %d sent to barber \n", customer_pid)
+		barberWorkingCh <- customer_pid
+
+		// if customer_pid {
+		// 	barberWorkingCh <- customer_pid
+		// } else if len(waitingRoomCh) == 0 {
+		// 	barberSleepCh <- true
+		// }
+	}
 
 }
 
 func barber() {
 	fmt.Printf("Barber created")
 	// Messages (receive_customer, cut_hair_random_time, tell customer_done)
-
+	for {
+		var customer_pid = <-barberWorkingCh
+		r := rand.Intn(20)
+		time.Sleep(time.Duration(r) * time.Second)
+		haircutComplete <- customer_pid
+	}
 }
